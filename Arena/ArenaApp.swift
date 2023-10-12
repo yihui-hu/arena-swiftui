2533c8b0e882b1923e41c6abdfaec50133d0330c
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-10-12 17:27:26 -0400

    Initial Commit

---
+++ Arena/ArenaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArenaApp.swift
+//  Arena
+//
+//  Created by Yihui Hu on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ArenaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
